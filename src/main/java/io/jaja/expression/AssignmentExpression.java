6b9c55f4273c8221f47679fc1bcb90d1485c7a9f
someh2705 <69467411+someh2705@users.noreply.github.com>
2024-05-12 19:32:50 +0900

    refactor: Improve test coverage in LexerTest (#15)
    * refactor: Improve test coverage in LexerTest
    
    Updated tests in LexerTest to cover literalTest and operatorTest.
    Refactored assertKind method to handle multiple expected TokenKinds.
    
    * feat: Add support for identifier and primitive tokens
    
    - Added new test method for identifier tokens
    - Added new test method for primitive tokens
    - Updated TokenTag enum with NAMED tag
    - Updated TokenKind enum with NAMED token pattern
    - Updated TokenKind enum with INT, EQ, and IDENTIFIER tokens
    
    * feat: Refactor ParserTest (#16)
    
    Combine import statements in PrinterTest and ParserTest for better code organization and readability.
    Refactor baseParserTest method in PrinterTest to complexExpressionTest in ParserTest for clarity and consistency.
    
    * refactor: Improve test coverage in LexerTest
    
    Updated tests in LexerTest to cover literalTest and operatorTest.
    Refactored assertKind method to handle multiple expected TokenKinds.
    
    * feat: Add support for identifier and primitive tokens
    
    - Added new test method for identifier tokens
    - Added new test method for primitive tokens
    - Updated TokenTag enum with NAMED tag
    - Updated TokenKind enum with NAMED token pattern
    - Updated TokenKind enum with INT, EQ, and IDENTIFIER tokens
    
    * feat: Add AssignmentExpression class and update Parser to support assignment operations
    
    Added a new AssignmentExpression class with required methods for handling assignment expressions.
    Updated Parser to support assignment expressions using the new class.
    Added tests for assignment expressions in ParserTest.

---
+++ src/main/java/io/jaja/expression/AssignmentExpression.java
@@ -0,0 +1,47 @@
+package io.jaja.expression;
+
+import io.jaja.Token;
+import io.jaja.utils.IteratorUtils;
+
+import java.util.Iterator;
+import java.util.Objects;
+
+public class AssignmentExpression implements Expression {
+    private Token identifier;
+    private Token operator;
+    private Expression expression;
+
+    public AssignmentExpression(Token identifier, Token operator, Expression right) {
+        this.identifier = identifier;
+        this.operator = operator;
+        this.expression = right;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AssignmentExpression that = (AssignmentExpression) o;
+        return Objects.equals(identifier, that.identifier) && Objects.equals(operator, that.operator) && Objects.equals(expression, that.expression);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(identifier, operator, expression);
+    }
+
+    @Override
+    public Expression lastChild() {
+        return expression;
+    }
+
+    @Override
+    public Iterator<Expression> iterator() {
+        return IteratorUtils.values(new PrimaryExpression(identifier), new PrimaryExpression(operator), expression);
+    }
+
+    @Override
+    public String toString() {
+        return "AssignmentExpression";
+    }
+}
--- src/test/java/io/jaja/LexerTest.java
+++ src/test/java/io/jaja/LexerTest.java
@@ -2,23 +2,46 @@ package io.jaja;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class LexerTest {
+    @Test
+    void literalTest() {
+        assertKind("10", TokenKind.INTLITERAL);
+    }
 
     @Test
-    void baseTokenTest() {
-        Lexer lexer = new Lexer("10 + (20 + 30) * 40");
+    void operatorTest() {
+        assertKind("()+ *", TokenKind.LPAREN, TokenKind.RPAREN, TokenKind.PLUS, TokenKind.STAR);
+    }
 
-        assertEquals(new Token(TokenKind.INTLITERAL, "10"), lexer.current());
-        assertEquals(new Token(TokenKind.PLUS, "+"), lexer.current());
-        assertEquals(new Token(TokenKind.LPAREN, "("), lexer.current());
-        assertEquals(new Token(TokenKind.INTLITERAL, "20"), lexer.current());
-        assertEquals(new Token(TokenKind.PLUS, "+"), lexer.current());
-        assertEquals(new Token(TokenKind.INTLITERAL, "30"), lexer.current());
-        assertEquals(new Token(TokenKind.RPAREN, ")"), lexer.current());
-        assertEquals(new Token(TokenKind.STAR, "*"), lexer.current());
-        assertEquals(new Token(TokenKind.INTLITERAL, "40"), lexer.current());
-        assertEquals(new Token(TokenKind.EOF, null), lexer.current());
+    @Test
+    void identifierTest() {
+        assertKind("number", TokenKind.IDENTIFIER);
+        assertKind("_number", TokenKind.IDENTIFIER);
+        assertKind("number1", TokenKind.IDENTIFIER);
+        assertKind("number1_number2", TokenKind.IDENTIFIER);
+        assertBadToken("1number");
+    }
+
+    @Test
+    void primitiveTokenTest() {
+        assertKind("int", TokenKind.INT);
+    }
+
+    private void assertKind(String string, TokenKind expected) {
+        assertSame(expected, new Lexer(string).current().kind);
+    }
+
+    private void assertKind(String string, TokenKind... expected) {
+        Lexer lexer = new Lexer(string);
+        for (TokenKind kind : expected) {
+            assertSame(kind, lexer.current().kind);
+        }
+    }
+
+    private void assertBadToken(String string) {
+        assertThrows(IllegalStateException.class, () -> new Lexer(string).current());
     }
 }

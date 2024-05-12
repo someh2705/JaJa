4d7206722da1fb60d5a687decc94a31920f36678
someh <someh2705@gmail.com>
2024-05-12 19:16:56 +0900

    feat: Refactor ParserTest
    Combine import statements in PrinterTest and ParserTest for better code organization and readability.
    Refactor baseParserTest method in PrinterTest to complexExpressionTest in ParserTest for clarity and consistency.

--- src/test/java/io/jaja/ParserTest.java
+++ src/test/java/io/jaja/ParserTest.java
@@ -1,35 +1,77 @@
 package io.jaja;
 
-import io.jaja.expression.AdditiveExpression;
-import io.jaja.expression.MultiplicativeExpression;
-import io.jaja.expression.ParenthesesExpression;
-import io.jaja.expression.PrimaryExpression;
+import io.jaja.expression.*;
+import io.jaja.utils.Printer;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParserTest {
+    private PrintStream oldOut;
+    private ByteArrayOutputStream out;
+
+    @BeforeEach
+    public void setUp() {
+        oldOut = System.out;
+        out = new ByteArrayOutputStream();
+        PrintStream newOut = new PrintStream(out);
+        System.setOut(newOut);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(oldOut);
+        oldOut = null;
+        out = null;
+    }
 
     @Test
-    void baseParserTest() {
+    void complexExpressionTest() {
         Parser parser = new Parser("10 + (20 + 30) * 40");
 
-        Token _10 = new Token(TokenKind.INTLITERAL, "10");
-        Token plus = new Token(TokenKind.PLUS, "+");
-        Token _20 = new Token(TokenKind.INTLITERAL, "20");
-        Token star = new Token(TokenKind.STAR, "*");
-        Token _30 = new Token(TokenKind.INTLITERAL, "30");
-        Token _40 = new Token(TokenKind.INTLITERAL, "40");
+        assertTreeOf(
+            parser.parseExpression(),
+            AdditiveExpression.class,
+                PrimaryExpression.class,
+                PrimaryExpression.class,
+                MultiplicativeExpression.class,
+                    ParenthesesExpression.class,
+                        AdditiveExpression.class,
+                            PrimaryExpression.class,
+                            PrimaryExpression.class,
+                            PrimaryExpression.class,
+                    PrimaryExpression.class,
+                    PrimaryExpression.class
+        );
+    }
 
-        PrimaryExpression __10 = new PrimaryExpression(_10);
-        PrimaryExpression __20 = new PrimaryExpression(_20);
-        PrimaryExpression __30 = new PrimaryExpression(_30);
-        PrimaryExpression __40 = new PrimaryExpression(_40);
+    private void assertTreeOf(Expression expression, Class<? extends Expression>... expected) {
+        List<String> names = new ArrayList<>();
 
-        AdditiveExpression additive1 = new AdditiveExpression(__20, plus, __30);
-        ParenthesesExpression parentheses = new ParenthesesExpression(additive1);
-        MultiplicativeExpression multiplicative = new MultiplicativeExpression(parentheses, star, __40);
-        AdditiveExpression additive2 = new AdditiveExpression(__10, plus, multiplicative);
+        for (Class<? extends Expression> clazz : expected) {
+            names.add(clazz.getSimpleName());
+        }
 
-        Assertions.assertEquals(additive2, parser.parseExpression());
+        assertTreeOf(expression, names);
+    }
+
+    private void assertTreeOf(Expression expression, List<String> expected) {
+        Printer.pretty(expression);
+        StringReader sr = new StringReader(out.toString());
+
+        try (BufferedReader br = new BufferedReader(sr)) {
+            for (String name : expected) {
+                assertContains(br.readLine(), name);
+            }
+        } catch (IOException e) { }
+    }
+
+    private void assertContains(String expected, String actual) {
+        Assertions.assertTrue(expected.contains(actual));
     }
 }

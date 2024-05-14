d8c46624bf92ef3c1c2959cde90bb968f8d5ccf2
someh <someh2705@gmail.com>
2024-05-14 23:35:42 +0900

    feat: Add line numbering and change output text colors

--- src/main/java/io/jaja/Shell.java
+++ src/main/java/io/jaja/Shell.java
@@ -4,8 +4,11 @@ import java.util.Scanner;
 
 public class Shell {
 
+    private static int line = 0;
+
     private static final String RED = "\u001b[31m";
     private static final String GREEN = "\u001b[32m";
+    private static final String YELLOW = "\u001b[33m";
     private static final String RESET = "\u001b[0m";
 
     public static void main(String[] args) {
@@ -14,12 +17,12 @@ public class Shell {
 
         do {
             try {
-                System.out.printf(RESET + "JaJa> ");
+                System.out.printf(RESET + "JaJa(" + line++ + ")> ");
                 String input = scanner.nextLine();
                 String result = runtime.evaluate(input);
-                System.out.println(GREEN + "JaJa> " + result);
+                System.out.println(YELLOW + "output: " + result);
             } catch (Diagnostics diagnostics) {
-                System.out.println(RED + diagnostics.getMessage());
+                System.out.println(RED + "error: " + diagnostics.getMessage());
             }
         } while (true);
     }

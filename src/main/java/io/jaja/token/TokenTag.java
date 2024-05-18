97159bd60210e0160507b19aee59f7b6fae4685e
someh2705 <69467411+someh2705@users.noreply.github.com>
2024-05-18 23:30:13 +0900

    feat: Added support for double literals (#27)

--- src/main/java/io/jaja/token/TokenTag.java
+++ src/main/java/io/jaja/token/TokenTag.java
@@ -3,26 +3,12 @@ package io.jaja.token;
 import java.util.regex.Pattern;
 
 public enum TokenTag {
-    _ZERO("0"),
-    _NON_ZERO_DIGIT("[1-9]"),
-    _JAVA_LETTER("[a-zA-Z$_]"),
-    _JAVA_LETTER_OR_DIGIT("[a-zA-Z0-9$_]"),
-    _NUMERIC("[0-9]"),
-
-    _DIGIT(_ZERO.or(_NON_ZERO_DIGIT)),
-
-    _DECIMAL_NUMERAL1(_ZERO.regex),
-    _DECIMAL_NUMERAL2(_NON_ZERO_DIGIT.regex + _DIGIT.exit()),
-
     WHITESPACE("[ \\t\\r\\n\\u000C]+"),
+    JAVA_IDENTIFIER("[a-zA-Z$_][a-zA-Z0-9$_]*"),
+    DECIMAL_NUMERAL("\\d+"),
+    FLOAT_NUMERAL("((\\d+)\\.?(\\d+)?f)|((\\d+)?\\.?(\\d+)f)"),
+    DOUBLE_NUMERAL("((\\d+)\\.(\\d+)?)|((\\d+)?\\.(\\d+))"),
     STRING("\".*\""),
-    DECIMAL_NUMERAL(_DECIMAL_NUMERAL1.or(_DECIMAL_NUMERAL2)),
-    JAVA_IDENTIFIER(_JAVA_LETTER.regex + _JAVA_LETTER_OR_DIGIT.star()),
-
-    DOT("."),
-    _FLOAT_NUMERAL1(DECIMAL_NUMERAL.exit() + DOT.exit() + DECIMAL_NUMERAL.exit() + "f"),
-    _FLOAT_NUMERAL2(DOT.regex + DECIMAL_NUMERAL.regex + "f"),
-    FLOAT_NUMERAL(_FLOAT_NUMERAL1.or(_FLOAT_NUMERAL2)),
     ;
 
     private final String regex;
